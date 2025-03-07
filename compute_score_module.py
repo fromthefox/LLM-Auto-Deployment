6b3974ce27d68b1954561015b24f344649379c3b
Yanhui Bian <fromthefox@icloud.com>
2025-03-07 18:03:00 +0800

    Update score_module

--- compute_score_module.py
+++ compute_score_module.py
@@ -3,4 +3,20 @@
 """
 
 def compute_score(arithmetic_list, bandwidth_list, memory_list):
-    
+    w1, w2, w3 = 0.4, 0.3, 0.3
+
+    max_arithmetic = max(arithmetic_list)
+    max_bandwidth = max(bandwidth_list)
+    max_memory = max(memory_list)
+
+    score_list = []
+
+    for i in range(len(arithmetic_list)):
+        normalized_arithmetic = arithmetic_list[i]/max_arithmetic
+        normalized_bandwidth = bandwidth_list[i]/max_bandwidth
+        normalized_memory = memory_list[i]/max_memory
+        score = w1 * normalized_arithmetic + w2 * normalized_bandwidth + w3 * normalized_memory
+        score_list.append(score)
+    return score_list
+
+
