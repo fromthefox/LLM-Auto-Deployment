f18ee1d9e5f06b1d396fce051d52b16b7d9d6cf2
Yanhui Bian <fromthefox@icloud.com>
2025-03-29 15:29:33 +0800

    Fix a bug

--- function_modules.py
+++ function_modules.py
@@ -12,12 +12,12 @@ def dict2list(nodes_info_dict:dict, network_matrix:list, central_node_index:int)
     this is a format converter, convert the initial topo format into the format we need to compute the node score.
     """
     network_list = network_matrix[central_node_index]
+    _ = network_list.pop(central_node_index) # remove the central node from the network list.
     nodes_arithmetic_list = []
     nodes_memory_list = []
     for i in range(len(network_list)):
-        if network_list[i] != 0: # if i == 0 means is the central node, we don't need to compute the score for it.
-            nodes_arithmetic_list.append(nodes_info_dict[i]["arithmetic"])
-            nodes_memory_list.append(nodes_info_dict[i]["memory"])
+        nodes_arithmetic_list.append(nodes_info_dict[i]["arithmetic"])
+        nodes_memory_list.append(nodes_info_dict[i]["memory"])
     
     res_dict = {
         "arithmetic": nodes_arithmetic_list,
